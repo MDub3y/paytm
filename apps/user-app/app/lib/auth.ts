dff2cc4f05904165f943b378ae4ecbcf0567477a
manasvi001 <manasvi.d007@gmail.com>
2025-01-08 19:31:48 +0530

    added_dockerfile

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -1,84 +1,68 @@
 import db from "@repo/db/client";
-import CredentialsProvider from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
-import { Account, NextAuthOptions, User } from "next-auth";
-import { AdapterUser } from "next-auth/adapters";
 
-interface Credentials{
-    phone: string,
-    password: string
-}
-
-export const authOptions: NextAuthOptions = {
+export const authOptions = {
     providers: [
-      CredentialsProvider({
-          name: 'Credentials',
-          credentials: {
-            phone: { label: "Phone number", type: "text", placeholder: "1231231231", required: true },
-            password: { label: "Password", type: "password", required: true }
-          },
-          // TODO: User credentials type from next-aut
-          async authorize(credentials: Credentials | undefined) {
-            // Do zod validation, OTP validation here
-            if(!credentials){ return null; }
-
-            const hashedPassword = await bcrypt.hash(credentials.password, 10);
-            const existingUser = await db.user.findFirst({
-                where: {
-                    number: credentials.phone
-                }
-            });
-
-            if (existingUser) {
-                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
-                if (passwordValidation) {
-                    return {
-                        id: existingUser.id.toString(),
-                        name: existingUser.name,
-                        number: existingUser.number
-                    }
-                }
-                return null;
-            }
-
-            try {
-                const user = await db.user.create({
-                    data: {
-                        number: credentials.phone,
-                        password: hashedPassword
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                phone: { label: "Phone number", type: "text", placeholder: "1231231231", required: true },
+                password: { label: "Password", type: "password", required: true }
+            },
+            // TODO: User credentials type from next-aut
+            async authorize(credentials: any) {
+                // Do zod validation, OTP validation here
+                const hashedPassword = await bcrypt.hash(credentials.password, 10);
+                const existingUser = await db.user.findFirst({
+                    where: {
+                        number: credentials.phone
                     }
                 });
-            
-                return {
-                    id: user.id.toString(),
-                    name: user.name,
-                    number: user.number
-                }
-            } catch(e) {
-                console.error(e);
-            }
 
-            return null
-          },
+                if (existingUser) {
+                    const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                    if (passwordValidation) {
+                        return {
+                            id: existingUser.id.toString(),
+                            name: existingUser.name,
+                            email: existingUser.number
+                        };
+                    }
+                    return null;
+                }
+
+                try {
+                    const user = await db.user.create({
+                        data: {
+                            number: credentials.phone,
+                            password: hashedPassword
+                        }
+                    });
+
+                    return {
+                        id: user.id.toString(),
+                        name: user.name,
+                        email: user.number
+                    } as any;
+                } catch (e) {
+                    console.error(e);
+                }
+
+                return null as any;
+            },
         })
     ],
+    // eslint-disable-next-line turbo/no-undeclared-env-vars
     secret: process.env.JWT_SECRET || "secret",
     callbacks: {
         // TODO: can u fix the type here? Using any is bad
         async session({ token, session }: any) {
-            session.user.id = token.sub
+            session.user.email = token.user?.email ?? ""; 
+            session.user.name = token.user?.name ?? "Unknown";
+            session.user.id = token.sub;
 
-            return session
-        },
-
-        async signIn({ user, account, profile }: { user: User | AdapterUser; account: Account | null; profile?: any }) {
-            // Ensure that the user email is defined
-            if (user?.id) {
-              // You can add your own signIn logic here (e.g., check the account provider)
-              return true;
-            }
-            return false; // Reject sign-in if user.email is not defined
-          }
+            return session;
+        }
     }
-  }
-  
+}
